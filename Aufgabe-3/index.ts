9edef78986cc2cb47345cba201181c78f3bff926
Luca Moser <mmlpv.moser@gmx.de>
2020-12-08 18:14:57 +0100

    Aufgabe 3.1

--- Aufgabe-3/index.ts
+++ Aufgabe-3/index.ts
@@ -1,26 +1,26 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.P_3_1Server = void 0;
-const Http = require("http");
-var P_3_1Server;
-(function (P_3_1Server) {
+import * as Http from "http";
+
+export namespace P_3_1Server {
     console.log("Starting server");
-    let port = Number(process.env.PORT);
+    let port: number = Number(process.env.PORT);
     if (!port)
         port = 8100;
-    let server = Http.createServer();
+
+    let server: Http.Server = Http.createServer();
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
     server.listen(port);
-    function handleListen() {
+
+    function handleListen(): void {
         console.log("Listening");
     }
-    function handleRequest(_request, _response) {
+
+
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
         console.log("I hear voices!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
         _response.end();
     }
-})(P_3_1Server = exports.P_3_1Server || (exports.P_3_1Server = {}));
-//# sourceMappingURL=index.js.map
+}
